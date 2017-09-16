c23e04f7ef60bffa932e4f80a08268698a4c5bdf
Donald Ng <donaldyann@gmail.com>
2017-09-16 13:20:48 +0800

    Fixed bug: missing total sum of trades.

--- run.py
+++ run.py
@@ -7,17 +7,14 @@ db = MongoClient().wtracker.trades
 def lookback(sec):
     t = int(time.time())-sec
 
-    print("")
-    print("[ Whaletracker ]")
-    print("")
+    print("\n[ Whaletracker ]\n")
     print("Tracking trades over past %s." % convert_to_time(sec))
     if global_price:
-        print("We are currently following %s coin, priced at %s%s." % (coin, currency, global_price))
-    print("")
+        print("We are currently following %s coin, priced at %s%s." % (pair, currency, global_price))
 
-    cur = db.find( { 'ts' : { '$gt' : 60 }, 'pair': coin } )
+    cur = db.find( { 'ts' : { '$gt' : 60 }, 'pair': pair } )
     tps = round(cur.count()/60, 4)
-    print("TPS: %s" % tps)
+    print("\nTPS: %s" % tps)
 
     min_amount = 99
     limit = 5
@@ -32,7 +29,7 @@ def lookback(sec):
             min_amount *= -1
 
         top_trades = db.aggregate([
-                                { "$match": { 'ts' : { '$gt' : t }, 'amount': { op : min_amount }, 'pair': coin } },
+                                { "$match": { 'ts' : { '$gt' : t }, 'amount': { op : min_amount }, 'pair': pair } },
                                 { "$group": {"_id": "$amount", "count": { "$sum": 1 }, "avgPrice": { "$avg": "$price" } }},
                                 { "$match": { 'count': {'$gt': 1}  }},
                                 { "$sort": { "count": -1 } },
@@ -41,11 +38,9 @@ def lookback(sec):
 
 
 
-        print("")
-        print("===========================")
-        print("===\tTop %s  \t===" % x.upper())
-        print("===========================")
-        print("")
+        print("\n===========================")
+        print("===\t  Top %s\t===" % x.upper())
+        print("===========================\n")
 
         c=0
         total = 0    
@@ -61,26 +56,28 @@ def lookback(sec):
 
             print("%s. %s x %s (%s)" % (c, avgPrice, amount, count))
 
+        if global_price and abs(total):
+            print("\nTotal: %s%s" % (currency, abs(round(total,2))))
+
 
 def spawn_tracker():
     pid=os.fork()
-    if pid==0: # new process
-        #print(x)
+    if pid==0:
         ppid = os.getpid()
-        os.system("nohup python3 tracker.py %s %s >/dev/null 2>&1 &" % (coin, ppid))
+        os.system("nohup python3 tracker.py %s %s >/dev/null 2>&1 &" % (pair, ppid))
         exit()
 
 
-def get_market_price(coin):
-    if len(coin)==6:
-        fsym = coin[:3]
-        tsym = coin[-3:]
+def get_market_price(pair):
+    if len(pair)==6:
+        fsym = pair[:3]
+        tsym = pair[-3:]
         url = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=%s" % (fsym, tsym)
 
         try:
             return float(requests.get(url).json()[tsym])
         except:
-            print("Can't find %s from coinmarketcap!" % coin)
+            print("Can't find %s from coinmarketcap!" % pair)
 
     return 0
 
@@ -105,24 +102,26 @@ def convert_to_time(seconds):
 def main():
 
     global global_price
-    global coin
+    global pair
     global currency
 
-    coin = str(sys.argv[1]).upper()
+    # Take 1st param as pair
+    pair = str(sys.argv[1]).upper()
 
-    # Default lookback timeframe will be set to 1 hour.
+    # Default lookback value
     lookback_seconds = 3600
 
+    # Take 2nd parameter as lookback value
     if len(sys.argv) == 3:
         lookback_seconds = int(sys.argv[2])
 
-    global_price = get_market_price(coin)
+    global_price = get_market_price(pair)
 
     if global_price:
 
         spawn_tracker()        
         
-        currency = '$' if coin[-3:] == 'USD' else '฿'
+        currency = '$' if pair[-3:] == 'USD' else '฿'
         
         while True:
             os.system("clear")
@@ -130,7 +129,7 @@ def main():
             print("\nprocessing... %s" % str(time.time())[-3:])
             time.sleep(1)
     else:
-        print("Error: cannot find %s pair." % coin)
+        print("Error: cannot find %s pair." % pair)
         sys.exit()
 
 
